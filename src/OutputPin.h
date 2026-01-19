01d89eaded115afc9a59382da23244d9fb0e6eb4
Alauddin Ansari <alauddinx27@gmail.com>
2026-01-19 21:54:13 +0530

    Add active low feature

--- src/OutputPin.h
+++ src/OutputPin.h
@@ -3,19 +3,26 @@
 
 #include "Arduino.h"
 
+const unsigned int BTN_UNDEFINED_PIN = 255;
+
 class OutputPin
 {
 public:
+  OutputPin();
   OutputPin(uint8_t pin);
+  OutputPin(uint8_t pin, bool activeLow);
+  void begin(uint8_t pin);
+  void begin(uint8_t pin, bool activeLow);
   void on();
   void off();
+  void toggle();
   void blink();
   void blink(uint8_t count);
   void blink(uint8_t count, uint16_t duration);
-  void toggle();
 
 private:
   uint8_t _pin;
+  bool _activeLow;
   bool _toggled;
 };
 
